887ff1fcaa9e415b6040b591f4f5ba4ba9bf036c
NatanSzymczak <natan.szymczak@gmail.com>
2022-08-19 15:14:29 +0200

    Update eslint and prettier config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-    globals: {
-        __PATH_PREFIX__: true,
-    },
-    extends: ['react-app', 'airbnb', 'prettier'],
-    rules: {
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'react/function-component-definition': [
-            2,
-            {
-                namedComponents: ['arrow-function', 'function-declaration'],
-                unnamedComponents: 'arrow-function',
-            },
-        ],
-    },
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    'react-app',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: ['arrow-function', 'function-declaration'],
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
 };
