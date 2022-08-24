abb96dfd960cff46fee6fc3cdde53ced696318d5
NatanSzymczak <natan.szymczak@gmail.com>
2022-08-24 22:40:35 +0200

    Add card component to articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,15 +1,44 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import MainLayouts from '../layouts';
 import Seo from '../components/seo';
 
-const ArticlesPage = () => (
-    <MainLayouts>
-        <h1>Hi from the Articles Page</h1>
-        <p>Welcome to Articles Page</p>
-        <Link to="/">Go back to the homepage</Link>
-    </MainLayouts>
-);
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+const ArticlesPage = () => {
+    return (
+        <MainLayouts>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={}
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                        >
+                            Card Title
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                        >
+                            Ullamco proident magna non nulla ex ullamco. Tempor adipisicing laboris occaecat aute Lorem culpa quis magna commodo dolore id aliquip. Commodo anim amet aliqua amet tempor elit aliquip exercitation culpa dolor quis dolor. Nulla duis duis ipsum aliqua adipisicing aliqua ullamco cupidatat proident occaecat enim ea est. Minim adipisicing exercitation elit enim deserunt tempor eu est esse culpa commodo. Mollit nisi incididunt adipisicing Lorem labore commodo dolore consectetur ex aliqua sint adipisicing et esse excepteur. Velit mollit pariatur est consectetur Lorem do proident cupidatat. Reprehenderit Lorem veniam pariatur mollit. Occaecat aliquip ut Lorem duis in cupidatat amet esse aliqua incididunt deserunt. Nostrud esse in id aliqua ad elit excepteur.
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </MainLayouts>
+    );
+};
 
 export const Head = () => <Seo title="Articles Page" />;
 
