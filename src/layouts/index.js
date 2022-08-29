bf37d427852c3cbc500a3aa1a18264c09c1e88b1
NatanSzymczak <natan.szymczak@gmail.com>
2022-08-29 19:38:11 +0200

    Use global styles in layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,15 @@
-import * as React from 'react'
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { darkTheme } from '../assets/styles/theme';
 import Navigation from '../components/Navigation/Navigation';
-import GlobalStyle from "../assets/styles/globalStyles";
+import { GlobalStyle } from '../assets/styles/globalStyles';
 
-const MainLayouts = ({children}) => (
-    <>
-        <GlobalStyle/>
-        <Navigation/>
+const MainLayouts = ({ children }) => (
+    <ThemeProvider theme={darkTheme}>
+        <Navigation />
+        <GlobalStyle />
         {children}
-    </>
-)
-
+    </ThemeProvider>
+);
 
 export default MainLayouts;
