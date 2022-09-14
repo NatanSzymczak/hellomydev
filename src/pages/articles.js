3816de72acd288234c4614bfb1ff6d1d91da0206
NatanSzymczak <natan.szymczak@gmail.com>
2022-09-14 21:09:41 +0200

    Wrap all posts

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -9,45 +9,58 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import slugify from 'slugify';
+import styled from 'styled-components';
+
+const PostListWrapper = styled.nav`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding-top: 20px;
+    //border: 1px solid red;
+`;
 
 const ArticlesPage = () => {
     const data = useStaticQuery(query);
 
     return (
         <MainLayouts>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <Link
-                        href={`articles/${slugify(data.allDatoCmsArticle.nodes[0].title, {
-                            lower: true,
-                        })}`}
-                        underline={'none'}
-                    >
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image={data.file.childImageSharp.gatsbyImageData.images.fallback.src}
-                            alt="green iguana"
-                        />
-                        <CardContent>
-                            <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="div"
-                            >
-                                Lizard
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color="text.secondary"
-                            >
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
-                        </CardContent>
-                    </Link>
-                </CardActionArea>
-            </Card>
+            <PostListWrapper>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <Link
+                            href={`articles/${slugify(data.allDatoCmsArticle.nodes[0].title, {
+                                lower: true,
+                            })}`}
+                            underline={'none'}
+                        >
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={
+                                    data.file.childImageSharp.gatsbyImageData.images.fallback.src
+                                }
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="div"
+                                >
+                                    Lizard
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="text.secondary"
+                                >
+                                    Lizards are a widespread group of squamate reptiles, with over
+                                    6,000 species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </Link>
+                    </CardActionArea>
+                </Card>
+            </PostListWrapper>
         </MainLayouts>
     );
 };
