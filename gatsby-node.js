96186dde9cfc985b952a02b4a6eb7f72b50658b6
NatanSzymczak <natan.szymczak@gmail.com>
2022-09-28 23:40:42 +0200

    Add result query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,21 @@
 const path = require(`path`);
 const slugify = require('slugify');
 
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
+    const result = await graphql(`
+        query queryCMSPage {
+            allDatoCmsArticle {
+                nodes {
+                    id
+                    image {
+                        url
+                    }
+                    title
+                }
+            }
+        }
+    `);
     createPage({
         path: '/using-dsg',
         component: require.resolve('./src/templates/using-dsg.js'),
