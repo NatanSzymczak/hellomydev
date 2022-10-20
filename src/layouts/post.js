d5e464699885a1e0dad5848d98c8466c73e5e124
NatanSzymczak <natan.szymczak@gmail.com>
2022-10-20 20:20:28 +0200

    Add query

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import Image from 'gatsby-image';
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+    query querySingleArticle($id: String!) {
+        datoCmsArticle(id: { eq: $id }) {
+            image {
+                fixed(width: 500) {
+                    ...GatsbyDatoCmsFixed_tracedSVG
+                }
+            }
+        }
+    }
+`;
 
 const PostLayout = ({ data }) => <Image fixed={data.datoCmsArticle.image.fixed} />;
 
